804e685c339b337a452ecb391df8681b07c3ae27
Earlo <theearlord@gmail.com>
2017-01-26 15:36:28 +0200

    graphical tweaks

--- wavetypes.js
+++ wavetypes.js
@@ -21,12 +21,12 @@
 						*/
 
 						/*
-						//Queen Waves
-						newval = ( (		self.oT[x  ][y+2] + 
-						self.oT[x-1][y+1] + self.oT[x  ][y+1] + self.oT[x+1][y+1] +
-	self.oT[x-2][y  ] +	self.oT[x-1][y  ] 					  + self.oT[x+1][y  ] + self.oT[x+2][y  ] +
-						self.oT[x-1][y-1] + self.oT[x  ][y-1] + self.oT[x+1][y-1] +
-											self.oT[x  ][y-2]						)/6 - self.ooT[x][y] ) | 0;
+					//Queen Waves
+						old[index] = ( (	cur[lu1+y ]  +
+							cur[lu0+y-1] + 	cur[lu0+y ]  + cur[lu0+y+1] +
+			cur[lc+y-2 ] + 	cur[lc+y-1 ] 				 + cur[lc+y+1 ] + cur[lc+y+2 ] +
+							cur[ld0+y-1] + 	cur[ld0+y ]  + cur[ld0+y+1] +
+											cur[ld1+y ]  						)/6 - old[index] ) | 0;
 						*/
 						/*
 						//Knight Waves
